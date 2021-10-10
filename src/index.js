65be58ec290c67aaa6f82944ef0bf8ec2f1f47a3
htphong01 <thanhphonga4.hbt@gmail.com>
2021-10-11 00:06:23 +0700

    create question by text

---
+++ src/index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
+app.get('/', (req, res, next) => {
+  res.send('Hello world')
+})
+
+const rooms = {
+  1: {
+    id: 1,
+    host: 123,
+    users: []
+  }
+} 
+
+io.on("connection", (socket) => {
+  socket.on('join', (data) => {
+    const { roomId } = data;
+    if(rooms.id) {
+      
+    } else {
+      const newRoom = {
+        id: roomId,
+        host: '',
+        users: []
+      }
+      rooms[roomId] = newRoom;
+      socket.join(roomId);
+    }
+  });
+
+  socket.on('newQuestion', data => {
+    const { roomId, ...rest } = data;
+    io.in(roomId).emit('newQuestion', rest);
+  })
+
+  socket.on('newAnswer', data => {
+    const { roomId, ...rest } = data;
+    socket.to(roomId).emit('newAnswer', rest);
+  }) 
+
+
+});
+
+httpServer.listen(4000);
